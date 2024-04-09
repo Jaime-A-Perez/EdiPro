3ef607dff614c9a5739cba9ee0a1e08ca1db4258
Jaime-A-Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2024-04-09 10:49:34 -0500

    Create DatasourceShedulingImpl to get data from json

---
+++ lib/infrastructure/datasource_impl/datasource_sheduling_imp.dart
@@ -0,0 +1,23 @@
+
+import 'dart:convert';
+
+import 'package:edi_pro/domain/datasources/datasource_sheduling.dart';
+import 'package:edi_pro/domain/entities/sheduling_entity.dart';
+import 'package:edi_pro/infrastructure/mapper/mapper_sheduling.dart';
+import 'package:edi_pro/infrastructure/models/model_sheduling.dart';
+import 'package:flutter/services.dart';
+
+class DatasourceShedulingImpl extends DatasourceSheduling {
+  @override
+  Future<ShedulingEntity> getSheduling() async {
+    final String response = await rootBundle.loadString('assets/json/edipro.json');
+    final data = await json.decode(response);
+    
+    final ModelSheduling dataModeling = await ModelSheduling.fromJson(data);
+
+    print(dataModeling);
+    return MapperSheduling.shedulingToShedulingEntity(dataModeling);
+  }
+
+
+}
