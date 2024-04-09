32df4323e6b7cba4064bd058b343c7f320b3c790
Jaime-A-Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2024-04-08 20:47:17 -0500

    Add margin to SectionHero

--- lib/presentation/widgets/section_hero.dart
+++ lib/presentation/widgets/section_hero.dart
@@ -13,8 +13,9 @@ class SectionHero extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = AppTheme().getTheme().textTheme;
-    return Padding(
-      padding: const EdgeInsets.symmetric( horizontal: 16.0),
+    return Container(
+      margin: const EdgeInsets.only(bottom: 8),
+      padding: const EdgeInsets.all( 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
