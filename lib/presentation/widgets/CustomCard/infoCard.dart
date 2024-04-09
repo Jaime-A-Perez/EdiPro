5ca0379a910fa03af88482f5f06614675944269b
Jaime-A-Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2024-04-08 23:59:30 -0500

    Add InfoCard stateless widget

---
+++ lib/presentation/widgets/CustomCard/infoCard.dart
@@ -0,0 +1,89 @@
+import 'package:edi_pro/presentation/widgets/CustomCard/service_chips.dart';
+import 'package:flutter/material.dart';
+
+class InfoCard extends StatelessWidget {
+  const InfoCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          _Ranking(
+              icon: Icons.star_border_purple500_rounded,
+              qualification: '4.9',
+              style: theme.textTheme.bodySmall!,
+          ),
+
+          _TitleAndSubtitle(
+            theme: theme.textTheme,
+            title: 'Terca Cooperativa Carpintería',
+            subtitle: "Let's design the furniture you need to harmonize your space.",
+          ),
+
+          ServiceChips(),
+        ],
+      ),
+    );
+  }
+}
+
+
+class _Ranking extends StatelessWidget {
+  final TextStyle style;
+  final IconData icon;
+  final String qualification;
+  const _Ranking({   
+    required this.icon, 
+    required this.qualification, 
+    required this.style
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+     children: [
+      Icon(icon,size: 21),
+      Text(qualification, style: style,)
+     ],
+    );
+  }
+}
+
+
+class _TitleAndSubtitle extends StatelessWidget {
+  final TextTheme theme;
+  final String title;
+  final String subtitle;
+  const _TitleAndSubtitle({    
+    required this.theme, 
+    required this.title, 
+    required this.subtitle
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(4.0),
+      child: Column(
+        
+        children: [
+          Text(
+            title, style: theme.titleMedium,
+            textAlign: TextAlign.center
+            ),
+          SizedBox(
+            height: 6,
+          ),
+          Text(
+            subtitle, style: theme.bodyMedium,
+            textAlign: TextAlign.center
+            )
+        ],
+      ),
+    );
+  }
+}
